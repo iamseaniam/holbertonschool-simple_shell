e0eb6929288a9a1154461e68c5a8342199d1081c
C:\Users\seana\OneDrive\Documents <sean.alexander613@gmail.com>
2023-11-28 14:23:01 -0600

    added code to main.c and main.h

--- main.c
+++ main.c
@@ -1,1 +1,31 @@
 #include "main.h"
+
+int main(void)
+{
+	char *buffer;
+	size_t buffsize = 100;
+	ssize_t line_size;
+	int id = fork();
+
+	buffer = (char *)malloc(buffsize * sizeof(char));
+	if (buffer == NULL)
+	{
+		perror("unable to allocate buffer");
+		exit(1);
+	}
+	while (1)
+	{
+		printf("Enter YOUR command ");
+		line_size = getline(&buffer, &buffsize, stdin);
+		fork();
+		if (id == 0)
+		{
+			if (line_size == -1)
+				break;
+			if (strcmp(buffer, "exit\n") == 0)
+				break;
+		}
+    }
+	free(buffer);
+	return 0;
+}
--- main.h
+++ main.h
@@ -3,5 +3,8 @@
 
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
 
 #endif
