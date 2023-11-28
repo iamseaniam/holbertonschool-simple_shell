568d421c86791ee53e0f4634fc7ba66a54f59390
NolbsAtHolbs <hikikomode@gmail.com>
2023-11-28 11:49:49 -0600

    included header files, spacing, betty formatting

--- main.c
+++ main.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include <string.h>
+#include <unistd.h>
 #include "main.h"
+/**
+ * function - 
+ * @variable1:
+ * @variable2:
+ * Return: expected command result
+*/
--- main.h
+++ main.h
@@ -5,4 +5,5 @@
 #include <unistd.h>
 
 
+
 #endif
