cc85197006d6f4e2a694f7f41f25a560f7ae5cc2
C:\Users\seana\OneDrive\Documents <sean.alexander613@gmail.com>
2023-12-07 20:37:11 -0600

    Eroors

--- check_exit.c
+++ check_exit.c
@@ -7,5 +7,11 @@
 */
 int check_exit(char *argv[])
 {
+	/*Ensure argv is not NULL*/
+	if (argv == NULL || argv[0] == NULL)
+	{
+		/* handle the error or return an apporiate value*/
+		return (0) /*assuming "exit" is not present if theres an issue*/
+	}
 	return (strcmp(argv[0], "exit") == 0);
 }
